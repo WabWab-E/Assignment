060677e91a4ed0d9b4e3056d8e513cb3abc9159f
WabWab-E <HBbang03@gmail.com>
2022-12-05 00:34:58 +0900

    update .ino file

--- Assignment_ino/Assignment_ino.ino
+++ Assignment_ino/Assignment_ino.ino
@@ -3,27 +3,27 @@ void setup(){
     pinMode(13, OUTPUT);
     }
 
-    double th(int v) {
-    double t;
+double th(int v) {
+  double t;
 
-    t = log(((10240000/v) - 10000));
-    t = 1 /(0.001129148 + (0.> 000234125*t) + (0.0000000876741*t*t*t));
-    t = t - 273.15;
+  t = log(((10240000/v) - 10000));
+  t = 1 /(0.001129148 + (0.> 000234125*t) + (0.0000000876741*t*t*t));
+  t = t - 273.15;
 
-    return t;
-    }
+  return t;
+}
 
-    int dly;
-    void loop(){
-    int a=analogRead(A0);
-    Serial.println(th(a));
+int dly;
+void loop(){
+  int a=analogRead(A0);
+  Serial.println(th(a));
 
-    if(Serial.available()>0){
-    dly = (Serial.readString()).toInt();
-    }
+  if(Serial.available()>0){
+  dly = (Serial.readString()).toInt();
+  }
 
-    digitalWrite(13, HIGH);
-    delay(dly);
-    digitalWrite(13, LOW);
-    delay(dly);
+  digitalWrite(13, HIGH);
+  delay(dly);
+  digitalWrite(13, LOW);
+  delay(dly);
 }
